314128174e6d0b1d75298786860d3e75622eaba3
Aviruk Basak <avirukbasak@gmail.com>
2021-02-27 19:10:58 +0530

    added files

--- src/main.c
+++ src/main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define RED "\e[31m"
+#define GRN "\e[32m"
+#define YEL "\e[33m"
+#define BLU "\e[34m"
+#define MAG "\e[35m"
+#define CYN "\e[36m"
+#define RST "\e[0m"
+
+#define VAL 10000000
 
 int main() {
-    
-    return 0;
+    printf("\e[?25l");
+    do {
+        printf(GRN "     ####\\\n");
+        printf(BLU "  <<<"GRN"#########\\\n");
+        printf(GRN "     #################\n");
+        printf(BLU "  <<<"GRN"#########/\n");  
+        printf(GRN "     ####/\n" RST);
+        int i = VAL; while (i-- > 0);
+        system("clear");
+        printf(GRN "     ####\\\n");
+        printf(GRN "     #########\\\n");
+        printf(GRN "     #################"RED">> "YEL"---      ---       ---      ---         ---\a\n");
+        printf(GRN "     #########/\n");  
+        printf(GRN "     ####/\n" RST);
+        i = VAL; while (i-- > 0);
+        system("clear");
+    } while (1);
+    printf("\e[?25h");
+    return 0;  
 }
